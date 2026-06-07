980a1a472a3db980b2e52c7d61a2943351f3ea76
HSC <1821343103@qq.com>
2026-06-07 14:51:31 +0800

    `用户模块`: 1、用户注册、登录、注销、获取登录用户 2、全局权限校验★ 3、用户管理 4、修复 MyBatis 分页和长整数精度丢失问题（一些小坑）

---
+++ src/main/java/com/hsc/hsmartpicbackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.hsc.hsmartpicbackend.mapper;
+
+import com.hsc.hsmartpicbackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author hsc
+ * @description 针对表【user(用户)】的数据库操作Mapper
+ * @createDate 2026-06-07 10:19:13
+ * @Entity com.hsc.hsmartpicbackend.model.entity.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/hsc/hsmartpicbackend/model/dto/user/UserAddRequest.java
@@ -0,0 +1,40 @@
+package com.hsc.hsmartpicbackend.model.dto.user;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户创建请求
+ */
+@Data
+public class UserAddRequest implements Serializable {
+
+    private static final long serialVersionUID = 7042159856893982471L;
+
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户角色: user, admin
+     */
+    private String userRole;
+
+}
